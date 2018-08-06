65d90b52ffe641d22293a9e530b84b9219f4d310
Chintan Patel <patelchin1@ecs.vuw.ac.nz>
2018-08-06 12:05:52 +1200

    finished categories functionality

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -11,6 +11,9 @@ import { CategoryViewPage } from '../category-view/category-view';
 export class CategoriesPage {
   public categoriesList = [];
   public itemsList = [];
+  public itemsInCategory;
+
+  public testArray = [];
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               public tasksProvider : TasksProvider) {
@@ -34,16 +37,59 @@ export class CategoriesPage {
 
     //populate the categories
     this.categoriesList = [];
-    for(let i = 0; i < this.itemsList.length; i ++){
-      let needle = this.itemsList[i].taskCategory.toLowerCase();
-      if(this.categoriesList.indexOf(needle) == -1){
-        this.categoriesList.push(needle);
+
+    for(let i = 0; i < this.itemsList.length; i++){
+      let itemCategory = this.itemsList[i].taskCategory.toLowerCase();
+      let itemMap = {
+        title : this.itemsList[i].taskCategory,
+        total : -1
+      };
+
+      if(!this.categoriesContains(itemCategory)){
+        itemMap.total = 1;
+        this.categoriesList.push(itemMap);
+      }else{
+        let indexToEdit = this.categoryIndex(itemCategory);
+        this.categoriesList[indexToEdit].total++;
       }
     }
-    console.log(this.categoriesList.length);
+
+
   }
 
-  openCategory(){
-    this.navCtrl.push(CategoryViewPage)
+
+  private categoryIndex(categoryTitle){
+    for(let i = 0; i < this.categoriesList.length; i++){
+      if(this.categoriesList[i].title === categoryTitle){
+        return i;
+      }
+    }
+    return -1;
   }
+
+  private categoriesContains(categoryTitle) : boolean{
+    for(let i = 0; i < this.categoriesList.length; i++){
+      if(this.categoriesList[i].title === categoryTitle){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  debugger(){
+    console.log("in debugger");
+    console.log(this.testArray.length);
+    console.log(this.testArray);
+  }
+
+  openCategory(categoryName){
+    this.navCtrl.push(CategoryViewPage, {
+      categoryName : categoryName
+    })
+  }
+
+
+
+   
+
 }
--- src/pages/category-view/category-view.ts
+++ src/pages/category-view/category-view.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TasksProvider } from '../../providers/tasks/task';
+import { TaskDetailPage } from '../task-detail/task-detail';
 
 /**
  * Generated class for the CategoryViewPage page.
@@ -13,12 +15,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'category-view.html',
 })
 export class CategoryViewPage {
+  public categoryName;
+  public itemsList = [];
+  public categoryItems = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public tasksProvider : TasksProvider) {
+    this.categoryName = navParams.get('categoryName');
+    this.populate();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CategoryViewPage');
+
+
   }
 
+
+  populate(){
+    //get all the items
+    this.tasksProvider.getTasksList().on("value", eventListSnapshot => {
+      this.itemsList = [];
+      eventListSnapshot.forEach(snap => {
+        this.itemsList.push({
+          id: snap.key,
+          taskTitle: snap.val().taskTitle,
+          taskDescription: snap.val().taskDescription,
+          taskDate: snap.val().taskDate,
+          taskCategory: snap.val().taskCategory
+        });
+      });
+    });
+
+    //filter items to get specific category
+    for(let i = 0; i < this.itemsList.length; i++){
+      if(this.itemsList[i].taskCategory === this.categoryName){
+        this.categoryItems.push(this.itemsList[i]);
+      }
+    }
+  }
+
+  goToTaskDetail(item, itemId){
+    console.log("Item ID is: " + itemId);
+    this.navCtrl.push(TaskDetailPage, {
+      item: item,
+      key: itemId
+    });
+  }
 }
